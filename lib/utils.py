009ff24b7b1330f1811ab0fbf8d4e63175ba4400
Eliana Harriet <eliana.harriet@innovid.com>
2024-11-09 19:28:33 -0300

    Add MongoDB dependencies and implement MongoDB client connection

--- lib/utils.py
+++ lib/utils.py
@@ -3,6 +3,9 @@ import os
 import time
 from typing import Optional, Union
 from sqlalchemy import create_engine
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+import logging as logger
 
 HOUR = 60 * 60
 MINUTE = 60
@@ -23,6 +26,12 @@ def get_engine() -> Optional[create_engine]:
 def get_test_engine():
     database_url = os.getenv('DATABASE_URL', 'sqlite:///test.db')  # Default to a SQLite database for testing
     return create_engine(database_url)
+
+def get_mongo_client() -> MongoClient:
+    uri = f"mongodb+srv://{os.getenv('MONGO_USER')}:{os.getenv('MONGO_PASSWORD')}@{os.getenv('MONGO_HOST')}/?retryWrites=true&w=majority&appName={os.getenv('MONGO_APP_NAME')}"
+    print(f"Connecting to MongoDB: {uri}")
+    logger.getLogger('pymongo').setLevel(logger.WARNING)
+    return MongoClient(uri, server_api=ServerApi('1'))
 
 def get_actual_time() -> str:
     return datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
